a035aec622252e14b38a0bcb6f7cdf63a8f9cb50
Malik-p <sahilmaik2005@gmail.com>
2025-06-14 17:58:52 +0530

    setup backend in my local-setup wiht all the changes

--- server/controller/domainController.js
+++
@@ -1,7 +0,0 @@
-const domains = require('../data/domain');
-
-const getAllDomains = (req,res) => {
-    res.status(200).json(domains);
-}
-
-module.exports = {getAllDomains};
--- server/routes/domainRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const {getAllDomains} = require('../controller/domainController');
-
-router.get('/',getAllDomains);
-
-module.exports = router;
