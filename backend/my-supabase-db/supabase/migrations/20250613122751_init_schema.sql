3933cbbf968e039ca12f7b839abb097c3931aba1
dilpreet_singh_gill <dilpreetg0707@gmail.com>
2025-06-14 23:24:54 +0530

    Prisma Setup

--- backend/my-supabase-db/supabase/migrations/20250613122751_init_schema.sql
+++
@@ -1,7 +0,0 @@
--- Example schema
-create table profiles (
-  id uuid primary key default gen_random_uuid(),
-  name text not null,
-  email text unique not null,
-  created_at timestamp default now()
-);
--- backend/my-supabase-db/supabase/migrations/20250613125130_add_new_table.sql
+++
@@ -1,5 +0,0 @@
-create table shiva_messages (
-  id serial primary key,
-  sender text default 'Shiva',
-  created_at timestamp default now()
-);
