1a6aa25970e061697b03c64f4ad5d5189565ac02
Nandini-kush <nandinikushwah7898@gmail.com>
2025-07-17 23:46:36 +0530

    Changes done

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,5 @@
+/////server.js
 import express from 'express';
-import cors from 'cors';
 import loader from './loader/index.js';
 import dotenv from 'dotenv';
 import cors from 'cors';
@@ -10,15 +10,6 @@ dotenv.config();
 // ✅ Initialize Express app
 const app = express();
 
-<<<<<<< HEAD
-app.use(cors({
-  origin: 'http://localhost:5173', // Your frontend URL
-  credentials: true ,// If using cookies/auth headers
-   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization']
-}));
-
-=======
 // ✅ CORS configuration
 app.use(cors({
   origin: 'http://localhost:5173', // frontend URL
@@ -28,7 +19,6 @@ app.use(cors({
 // ✅ Body parser (json)
 app.use(express.json());
 
->>>>>>> b0cd3b99a5687a8e006efa9225a83517263d02b5
 const startServer = async () => {
   await loader(app); // Initialize routes, middlewares etc.
 
@@ -39,12 +29,9 @@ const startServer = async () => {
   });
 
   // ✅ Start the server
-<<<<<<< HEAD
-  const PORT = process.env.PORT || 5000;
-=======
   const PORT = process.env.PORT || 8080; // default to 5000
->>>>>>> b0cd3b99a5687a8e006efa9225a83517263d02b5
   app.listen(PORT, () => console.log(`🚀 Server running on port ${PORT}`));
 };
 
 startServer();
+
