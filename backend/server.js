1b6a2d3cc1b938287406fb0809353edfe779eab6
Anjaligarhwal <anjaligarhwal1610@gmail.com>
2025-07-30 23:35:56 +0530

    changes in server.js

--- backend/server.js
+++ backend/server.js
@@ -17,35 +17,38 @@ app.get("/", (req, res) => {
   });
 
 
-// ✅ CORS configuration
 
+// ✅ CORS configuration
 const prodOrigins = [
   process.env.ORIGIN_1,
   process.env.ORIGIN_2,
   process.env.ORIGIN_3,
-].filter(Boolean);
+].filter(Boolean); // remove undefined entries
 
 const devOrigin = ['http://localhost:5173'];
-const allowedOrigins = getEnvironmentVariable('NODE_ENV') === 'production' ? prodOrigins : devOrigin;
+
+const allowedOrigins = process.env.NODE_ENV === 'production' ? prodOrigins : devOrigin;
+
 app.use(
   cors({
     origin: (origin, callback) => {
-      if (getEnvironmentVariable('NODE_ENV') === 'production') {
+      if (process.env.NODE_ENV === 'production') {
         if (!origin || allowedOrigins.includes(origin)) {
           callback(null, true);
         } else {
-          callback(new Error(`${origin} not allowed by cors`));
+          callback(new Error(`${origin} not allowed by CORS`));
         }
       } else {
         callback(null, true);
       }
     },
-     credentials: true,
+    credentials: true,
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
     allowedHeaders: ['Content-Type', 'Authorization'],
     optionsSuccessStatus: 200,
   }),
 );
+
 
 // Body parser (json)
 app.use(express.json());
