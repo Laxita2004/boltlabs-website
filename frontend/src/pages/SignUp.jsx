8981b43676b59f88566e92ebb9225df57d05d64f
Anjaligarhwal <anjaligarhwal1610@gmail.com>
2025-07-30 20:00:34 +0530

    change sign up page

--- frontend/src/pages/SignUp.jsx
+++ frontend/src/pages/SignUp.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import { User, Mail, Lock, Eye, EyeOff } from 'lucide-react';
 import axios from '../api/axios';
-import { BACKEND_URL } from '../../config/config';
+import { BACKEND_URL } from '../config/config';
 
 const SignUp = () => {
   const navigate = useNavigate();
